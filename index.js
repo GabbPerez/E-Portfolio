a4fdc96ac75b95f6af452af161df56e3d37c05d1
Gabriel <gabgabcperez@gmail.com>
2022-02-19 17:33:49 +1100

    v1 without links added

---
+++ index.js
@@ -0,0 +1,59 @@
+//template_pbcrm0r
+//service_965b7im
+//user_xCtwFxrwN9qztgVoznqK
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1/20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; i++){
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
+toggleContrast = () => {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle){
+        document.body.classList += " dark-theme";
+    } else {
+        document.body.classList.remove("dark-theme");
+    }
+}
+
+contact = (event) => {
+    event.preventDefault(); 
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += ' modal__overlay--visible';
+
+    emailjs
+        .sendForm(
+            'service_965b7im',
+            'template_pbcrm0r',
+        event.target,
+             'user_xCtwFxrwN9qztgVoznqK'
+    ).then(() =>{
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += ' modal__overlay--visible';
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly on gabgabcperez@gmail.com"
+        );
+    })
+}
+
+toggleModal = () => {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open"
+}
